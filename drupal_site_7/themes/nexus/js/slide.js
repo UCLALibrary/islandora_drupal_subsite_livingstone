ea8042c38a347c92ced7bcde9af885f0911e45b3
pghorpade <pghorpade@library.ucla.edu>
2014-07-16 07:20:58 -0700

    changed animation from fade to slide

--- drupal_site_7/themes/nexus/js/slide.js
+++ drupal_site_7/themes/nexus/js/slide.js
@@ -1,11 +1,14 @@
 jQuery(window).load(function() {
 
   jQuery('#slidebox').flexslider({
-    animation: "fade",
+    animation: "slide",
+    direction: "horizontal",
     directionNav:true,
     controlNav:false
   });
    jQuery('.flexslider').flexslider({
+     animation: "slide",
+   /* direction: "horizontal",   */
     controlNav: true,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
     directionNav: false 
   }); 
