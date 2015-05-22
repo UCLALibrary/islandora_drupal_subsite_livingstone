33104ce5b0904a2144200e7f693a99dcfcd546ce
kappaluppa <kappaluppa2@gmail.com>
2015-05-22 15:21:36 -0700

    add more font choices

--- drupal_site_7/themes/nexus/templates/html.tpl.php
+++ drupal_site_7/themes/nexus/templates/html.tpl.php
@@ -5,7 +5,7 @@
 <title><?php print $head_title; ?></title>
 <?php print $styles; ?>
 <?php print $scripts; ?>
-<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic|Ramaraja|Crimson+Text:400,400italic|Pontano+Sans|Julius+Sans+One|Nobile:400,400italic|Patua+One|Raleway' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic|Ramaraja|Crimson+Text:400,600,700,400italic,600italic|Pontano+Sans|Julius+Sans+One|Nobile:400,400italic|Patua+One|Raleway' rel='stylesheet' type='text/css'>
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <!--[if lt IE 9]><script src="<?php print base_path() . drupal_get_path('theme', 'nexus') . '/js/html5.js'; ?>"></script><![endif]-->
 </head>
