d15aec09e4e5b554f621cb58106c4f67dbf1bd3a
pghorpade <pghorpade@library.ucla.edu>
2015-08-14 09:42:16 -0700

    Fixed empty table cells issue

--- drupal_site_7/themes/nexus/templates/islandora-solr-table.tpl.php
+++ drupal_site_7/themes/nexus/templates/islandora-solr-table.tpl.php
@@ -25,7 +25,7 @@
       <tr>
       <?php foreach($row as $key => $value):?>
           
-        <td class="<?php print $key; ?> show-table-cells">
+        <td class="<?php print $key; ?>">
             <?php if(strlen($value) == 0): ?>
             &nbsp;
                <?php else: ?> 
